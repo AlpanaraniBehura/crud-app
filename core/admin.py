ca174df1ad0195aed591795bfd684bb827ca5092
Alpanarani <alpanaranib@mirakitech.com>
2025-07-21 20:59:06 +0530

    Updated project

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.contrib import admin
 from .models import Employee
 
-admin.site.register(Employee)
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'position']  # Fields to show in list view
+    list_display_links = ['name']        # Makes 'name' clickable for edit
